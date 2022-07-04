f5fa336f98edd74d5e902d88fafdd1d770a66679
Huz <35820756+HuzPro@users.noreply.github.com>
2022-07-04 11:30:58 +0500

    Update SiteScraper.py

--- SiteScraper.py
+++ SiteScraper.py
@@ -1,14 +1,13 @@
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 import requests
-from openpyxl import Workbook, load_workbook
+from openpyxl import Workbook
 
 progName = "Artificial Intelligence Jobs"
 wb = Workbook()
 ws = wb.active
 ws.title = progName
 
-AISearchURL = "https://www.overseasjobs.com/job/search?Action=Search&keyword=Artificial%20Intelligence&country=US&location="
-AISearchURL2 = "https://www.adzuna.com/search?q=Artificial%20Intelligence&loc=151946&ac_where=1"
+AISearchURL = "https://www.overseasjobs.com/job/search?keyword=Artificial%20Intelligence&Action=Search&country=US&location=&p=1"
 
 r = requests.get(AISearchURL)
 soup = BeautifulSoup(r.content, "html5lib")
@@ -17,19 +16,20 @@ numberOfJobs = 20
 
 
 jobTitles = soup.find_all("a", attrs={"data-job-source":True})
-#jobEmployer = soup.find_all("a", attrs={"title":"Job Search", "href":True} )
 jobEmployer = soup.find_all("a", attrs={"href":True, "target":"_blank"})
+jobLocation = soup.find_all("div", attrs={"class":"job-location"})
+jobPosted = soup.find_all("footer",limit=20)
 
 
-for item in jobEmployer:
-    print(str(item['href'])+"\n")
 
-print(len(jobEmployer))
-
+jobPostedList = []
 jobTitleList = []
 jobEmployerList = []
 jobLinkList = []
-fileHeaders = ["Job Titles", "Link Of Employer", "Link Of Job"]
+jobLocationList = []
+
+
+fileHeaders = ["Job Titles", "Time posted/Posted by", "Job Location", "Link Of Job Source", "Link Of Job Post"]
 
 
 for i in range(numberOfJobs*2):
@@ -40,16 +40,23 @@ for i in range(numberOfJobs*2):
 
 for i in range(numberOfJobs):
     jobTitleList.append(str(jobTitles[i]['title']))
+    jobLocationList.append(str(jobLocation[i].string))
+    jobPostedList.append(str(jobPosted[i].string).replace("Posted: ","").replace("Posted on: ", "").replace("Posted on ", "").replace("\n","").replace("\t\t",""))
+    if jobLocationList[i] == "None": jobLocationList[i] = "NA"
+    
 
 
 
 
 ws.append(fileHeaders)
 
-for i, row in enumerate(ws.iter_rows(min_row=1, max_row=numberOfJobs, max_col=3)):
-    for cell in row:
-        cell.value = jobTitleList[i]
-        cell.value = jobEmployerList[i]
-        cell.value = jobLinkList[i]
+for i, col in enumerate(ws.iter_cols(min_row=2, max_row=numberOfJobs+1, max_col=5)):
+    for j, cell in enumerate(col):
+        if cell.column == 1: cell.value = jobTitleList[j]
+        if cell.column == 2: cell.value = jobPostedList[j]
+        if cell.column == 3: cell.value = jobLocationList[j]
+        if cell.column == 4: cell.value = jobEmployerList[j]
+        if cell.column == 5: cell.value = jobLinkList[j]
 
 wb.save(progName+".xlsx")
+print("Done!!! :D")
